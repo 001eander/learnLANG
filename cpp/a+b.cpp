5130e4871c4bab3f9278e259f2cb21f29872708f
oleander <yrr.tt@qq.com>
2024-09-14 16:48:09 +0800

    修改a+b，添加快速io方法

--- cpp/a+b.cpp
+++ cpp/a+b.cpp
@@ -1,14 +1,43 @@
 #include <iostream>
+#include <cstdio>
 
-using namespace std;
+namespace IO
+{
+    inline int read()
+    {
+        int x = 0, f = 1;
+        char c = getchar();
+        while (!isdigit(c))
+        {
+            if (c == '-')
+                f = -1;
+            c = getchar();
+        }
+        while (isdigit(c))
+        {
+            x = x * 10 + (c ^ 48), c = getchar();
+        }
+        return x * f;
+    }
+    void print(int x, char nxt = ' ')
+    {
+        printf("%d%c", x, nxt);
+    }
+
+    void endl()
+    {
+        putchar('\n');
+    }
+
+    void println(int x)
+    {
+        printf("%d\n", x);
+    }
+}
 
 int main()
 {
-    int a = 0, b = 0;
-
-    cin >> a >> b;
-
-    cout << a + b << endl;
-
+    int a = IO::read(), b = IO::read();
+    IO::println(a + b);
     return 0;
 }
